54446349c46d98410ad53a7b84c11f4be741127e
vzk92209-dot <vzk92209@gmail.com>
2026-06-10 00:33:38 +0500

    Added image array rendering to Header and built new Steps component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,6 +21,19 @@ const Header = () => {
             Generate Images 
             <img className="h-6" src={assets.star_group} alt="" />
         </button>
+
+        <div className="flex flex-wrap justify-center mt-16 gap-3">
+            {Array(6).fill('').map((item, index) => (
+                <img 
+                    className="rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10" 
+                    src={index % 2 === 0 ? assets.sample_img_2 : assets.sample_img_1} 
+                    alt="" 
+                    key={index} 
+                    width={70} 
+                />
+            ))}
+        </div>
+        <p className="mt-2 text-neutral-600">Generated images from imagify</p>
     </div>
   );
 }

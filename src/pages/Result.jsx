be26689391f174b51af7bb2c4dbfb81e097fd444
vzk92209-dot <vzk92209@gmail.com>
2026-06-10 02:25:43 +0500

    Animated Result/BuyCredit/Login views, fixing Login modal scroll position

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { assets } from '../assets/assets';
+import { motion } from 'framer-motion';
 
 const Result = () => {
   const [image, setImage] = useState(assets.sample_img_1);
@@ -12,7 +13,14 @@ const Result = () => {
   };
 
   return (
-    <form onSubmit={onSubmitHandler} className="flex flex-col min-h-[90vh] justify-center items-center">
+    <motion.form 
+        initial={{ opacity: 0.2, y: 100 }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        onSubmit={onSubmitHandler} 
+        className="flex flex-col min-h-[90vh] justify-center items-center"
+    >
       <div>
         <div className="relative">
             <img src={image} alt="" className="max-w-sm rounded" />
@@ -42,7 +50,7 @@ const Result = () => {
               <a href={image} download className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer">Download</a>
           </div>
       )}
-    </form>
+    </motion.form>
   );
 };
 

4d9e6c1696ca5ca92d6ed5efba789aa8a5b33e15
vzk92209-dot <vzk92209@gmail.com>
2026-06-10 01:00:15 +0500

    Added bottom CTA, generic Footer, and mocked Result page layout with animated loading state

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { assets } from '../assets/assets';
 
 const Result = () => {
   return (
-    <div>
-      Result Page
-    </div>
+    <form className="flex flex-col min-h-[90vh] justify-center items-center">
+      <div>
+        <div className="relative">
+            <img src={assets.sample_img_1} alt="" className="max-w-sm rounded" />
+            <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]"></span>
+        </div>
+        <p className="">Loading...</p>
+      </div>
+
+      <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
+        <input 
+            type="text" 
+            placeholder="Describe what you want to generate" 
+            className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color"
+        />
+        <button type="submit" className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full">
+            Generate
+        </button>
+      </div>
+    </form>
   );
 };
 

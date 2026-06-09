4444296a82dfbcab3da0b7a8288718f4c0756b14
vzk92209-dot <vzk92209@gmail.com>
2026-06-10 01:13:57 +0500

    Added state variables and conditional rendering logic to Result component for handling image generation flow

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { assets } from '../assets/assets';
 
 const Result = () => {
+  const [image, setImage] = useState(assets.sample_img_1);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState('');
+
+  const onSubmitHandler = async (e) => {
+      // Logic for generating image to be added
+      e.preventDefault();
+  };
+
   return (
-    <form className="flex flex-col min-h-[90vh] justify-center items-center">
+    <form onSubmit={onSubmitHandler} className="flex flex-col min-h-[90vh] justify-center items-center">
       <div>
         <div className="relative">
-            <img src={assets.sample_img_1} alt="" className="max-w-sm rounded" />
-            <span className="absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]"></span>
+            <img src={image} alt="" className="max-w-sm rounded" />
+            <span className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${loading ? 'w-full transition-all duration-[10s]' : 'w-0'}`}></span>
         </div>
-        <p className="">Loading...</p>
+        <p className={!loading ? 'hidden' : ''}>Loading...</p>
       </div>
 
-      <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
-        <input 
-            type="text" 
-            placeholder="Describe what you want to generate" 
-            className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color"
-        />
-        <button type="submit" className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full">
-            Generate
-        </button>
-      </div>
+      {!isImageLoaded && (
+          <div className="flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full">
+            <input 
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                type="text" 
+                placeholder="Describe what you want to generate" 
+                className="flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color"
+            />
+            <button type="submit" className="bg-zinc-900 px-10 sm:px-16 py-3 rounded-full">
+                Generate
+            </button>
+          </div>
+      )}
+
+      {isImageLoaded && (
+          <div className="flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full">
+              <p onClick={() => setIsImageLoaded(false)} className="bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer">Generate Another</p>
+              <a href={image} download className="bg-zinc-900 px-10 py-3 rounded-full cursor-pointer">Download</a>
+          </div>
+      )}
     </form>
   );
 };
